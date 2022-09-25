edcf47c76780cb1cb25f968a70dc4ed0d8f236b2
egargan <edwgargan@gmail.com>
2022-09-25 17:53:13 +0100

    feat(nvim): auto-install TS for usual langs

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,14 @@
 -- Plugin dev-oriented tree sitter library
 
+function setup()
+  require('nvim-treesitter.configs').setup({
+    ensure_installed = { "svelte", "typescript", "lua" },
+  })
+end
+
 return {
   name = 'nvim-treesitter/nvim-treesitter',
   options = { ['do'] = ':TSUpdate' },
+  setup = setup,
   priority = 2,
 }
