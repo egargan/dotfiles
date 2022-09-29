06718cda02fdb264f4dbc9a09375023756d98aa8
egargan <edwgargan@gmail.com>
2022-09-30 00:33:36 +0100

    [WIP] feat(nvim): use fzf-lua instead of telescope
    TODO: fix colors and layout for binds

---
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,89 @@
+-- In-nvim FZF
+
+local function setup()
+  local fzf_lua = require('fzf-lua')
+  local actions = require('fzf-lua.actions')
+
+  local opts = { noremap = true, silent = true }
+
+  -- General -------------------------------------------------------------------
+
+  -- Fuzzy find filenames
+  vim.keymap.set('n', '<C-t>', function() fzf_lua.files({
+    winopts = {
+      width = 60,
+      height = 40,
+      preview = {
+        layout = 'vertical',
+      }
+    },
+  }) end, opts)
+  -- Fuzzy find filenames, including ignored files
+  vim.keymap.set('n', '<S-t>', function() fzf_lua.files({
+    rg_opts = '--color=never --files --hidden --no-ignore-vcs -g "!.git"',
+  }) end, opts)
+
+  -- Fuzzy find text
+  vim.keymap.set('n', '\\', function() fzf_lua.grep() end, opts)
+  -- Fuzzy find text, including ignored files
+  vim.keymap.set('n', '|', function() fzf_lua.grep({
+    rg_opts = '--color=never --files --hidden --no-ignore-vcs -g "!.git"',
+  }) end, opts)
+
+  -- TODO: can we just fill the prompt vs this weird invisible grep?
+  -- Fuzzy find text under cursor
+  vim.keymap.set('v', '\\', function() fzf_lua.grep_visual() end, opts)
+  -- Fuzzy find text, including ignored files
+  vim.keymap.set('v', '|', function() fzf_lua.grep_visual({
+    rg_opts = '--color=never --files --hidden -g "!.git" --no-ignore-vcs'
+  }) end, opts)
+
+  -- Fuzzy find text in current buffer
+  vim.keymap.set('v', '<leader>f/', function() fzf_lua.grep_curbuf() end, opts)
+
+  -- Fuzzy find buffer list
+  vim.keymap.set('n', '<leader>fb', function() fzf_lua.buffers({
+    actions = {
+      ["ctrl-d"] = { actions.buf_del, actions.resume },
+      ["ctrl-h"] = function(selected)
+        for _, selection in ipairs(selected) do
+          print(selection)
+          local buffer = tonumber(selection:match("%[(%d+)"))
+          -- TODO: can we do this with the buffer index directly?
+          require('harpoon.mark').add_file(vim.api.nvim_buf_get_name(buffer))
+        end
+
+        require('harpoon.ui').toggle_quick_menu()
+      end
+    }
+  }) end, opts)
+
+
+  -- Git ----------------------------------------------------------------------
+
+  vim.keymap.set('n', '<leader>fh', function() fzf_lua.git_bcommits() end, opts)
+
+
+  -- LSP ----------------------------------------------------------------------
+
+  -- LSP diagnostics
+  vim.keymap.set('n', '<leader>fd', function() fzf_lua.diagnostics_document() end, opts)
+
+  -- LSP references of name under cursor
+  vim.keymap.set('n', '<leader>fr', function() fzf_lua.lsp_references() end, opts)
+
+
+  -- Highlights ----------------------------------------------------------------
+
+  vim.api.nvim_set_hl(0, "TelescopeBorder", { link = "Comment" })
+  vim.api.nvim_set_hl(0, "TelescopeResultsLineNr", { link = "Comment" })
+  vim.api.nvim_set_hl(0, "TelescopeResultsIdentifier", { link = "Comment" })
+  vim.api.nvim_set_hl(0, "TelescopeResultsNumber", { link = "Comment" })
+  vim.api.nvim_set_hl(0, "TelescopeMatching", { link = "Annotation" })
+end
+
+return {
+  name = 'ibhagwan/fzf-lua',
+  setup = setup,
+  priority = 1,
+}
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -47,76 +47,77 @@ function setup()
   require("telescope").load_extension("file_browser")
 
   -- Fuzzy find filenames
-  vim.keymap.set('n', '<C-t>', function() builtins.find_files({
-    -- TODO: make this a custom strategy,
-    layout_strategy = 'vertical',
-    layout_config = { width = 80, height = 0.5 },
-    previewer = false,
-  }) end, opts)
+  -- vim.keymap.set('n', '<C-t>', function() builtins.find_files({
+  --   -- TODO: make this a custom strategy,
+  --   layout_strategy = 'vertical',
+  --   layout_config = { width = 80, height = 0.5 },
+  --   previewer = false,
+  -- }) end, opts)
+
   -- Fuzzy find filenames, including ignored files
-  vim.keymap.set('n', '<S-T>', function() builtins.find_files({
-    layout_config = { width = 80, height = 0.5 },
-    layout_strategy = 'vertical',
-    vimgrep_arguments = vimgrep_args_no_ignore,
-    previewer = false,
-  }) end, opts)
+  -- vim.keymap.set('n', '<S-T>', function() builtins.find_files({
+  --   layout_config = { width = 80, height = 0.5 },
+  --   layout_strategy = 'vertical',
+  --   vimgrep_arguments = vimgrep_args_no_ignore,
+  --   previewer = false,
+  -- }) end, opts)
 
   -- Fuzzy find text
-  vim.keymap.set('n', '<leader>fg', builtins.live_grep, opts)
-  -- Fuzzy find text, including ignored file
-  vim.keymap.set('n', '<leader>fG', function() builtins.live_grep({
-    vimgrep_arguments = vimgrep_args_no_ignore,
-  }) end, opts)
-  -- Fuzzy find text, pre-filled with selection
-  -- TODO: make --no-ignore version
-  vim.keymap.set('v', '<leader>fg', '"zy:Telescope live_grep default_text=<C-r>z<cr>', opts)
+  -- vim.keymap.set('n', '<leader>fg', builtins.live_grep, opts)
+  -- -- Fuzzy find text, including ignored file
+  -- vim.keymap.set('n', '<leader>fG', function() builtins.live_grep({
+  --   vimgrep_arguments = vimgrep_args_no_ignore,
+  -- }) end, opts)
+  -- -- Fuzzy find text, pre-filled with selection
+  -- -- TODO: make --no-ignore version
+  -- vim.keymap.set('v', '<leader>fg', '"zy:Telescope live_grep default_text=<C-r>z<cr>', opts)
 
   -- Fuzzy find text in buffer
-  vim.keymap.set('n', '<leader>f/', builtins.current_buffer_fuzzy_find, opts)
+  -- vim.keymap.set('n', '<leader>f/', builtins.current_buffer_fuzzy_find, opts)
 
-  -- Blame current buffer
-  vim.keymap.set('n', '<leader>fh', builtins.git_bcommits, opts)
+  -- -- Blame current buffer
+  -- vim.keymap.set('n', '<leader>fh', builtins.git_bcommits, opts)
 
   -- Fuzzy search buffers
   -- TODO: add multi select resolve for harpoon
-  vim.keymap.set('n', '<leader>fb', function() builtins.buffers({
-    layout_strategy = 'vertical',
-    layout_config = { width = 80, height = 20 },
-    previewer = false,
-    attach_mappings = function(_, map)
-      map("i", "<C-h>", function(prompt_bufnr)
-        local selected_buffers = {}
+  -- vim.keymap.set('n', '<leader>fb', function() builtins.buffers({
+  --   layout_strategy = 'vertical',
+  --   layout_config = { width = 80, height = 20 },
+  --   previewer = false,
+  --   attach_mappings = function(_, map)
+  --     map("i", "<C-h>", function(prompt_bufnr)
+  --       local selected_buffers = {}
 
-        action_utils.map_selections(prompt_bufnr, function(entry)
-          table.insert(selected_buffers, entry.value)
-        end)
+  --       action_utils.map_selections(prompt_bufnr, function(entry)
+  --         table.insert(selected_buffers, entry.value)
+  --       end)
 
-        local add_file = require('harpoon.mark').add_file
+  --       local add_file = require('harpoon.mark').add_file
 
-        if #selected_buffers > 0 then
-          for _, filename in pairs(selected_buffers) do
-            add_file(filename)
-          end
-        else
-          add_file(action_state.get_selected_entry().filename)
-        end
+  --       if #selected_buffers > 0 then
+  --         for _, filename in pairs(selected_buffers) do
+  --           add_file(filename)
+  --         end
+  --       else
+  --         add_file(action_state.get_selected_entry().filename)
+  --       end
 
-        require('telescope.actions').close(prompt_bufnr)
-        require('harpoon.ui').toggle_quick_menu()
-      end)
-      return true
-    end,
-  }) end, opts)
+  --       require('telescope.actions').close(prompt_bufnr)
+  --       require('harpoon.ui').toggle_quick_menu()
+  --     end)
+  --     return true
+  --   end,
+  -- }) end, opts)
 
   -- LSP Bindings ---------------------------------------------------------------------------------
 
-  vim.keymap.set('n', '<leader>fd', builtins.diagnostics, opts)
+  -- vim.keymap.set('n', '<leader>fd', builtins.diagnostics, opts)
 
-  vim.keymap.set('n', '<leader>fr', function() builtins.lsp_references({
-    layout_strategy = 'vertical',
-    layout_config = { width = 0.6, height = 0.75 },
-    show_line = false,
-  }) end, opts)
+  -- vim.keymap.set('n', '<leader>fr', function() builtins.lsp_references({
+  --   layout_strategy = 'vertical',
+  --   layout_config = { width = 0.6, height = 0.75 },
+  --   show_line = false,
+  -- }) end, opts)
 
   -- Highlights -----------------------------------------------------------------------------------
 
