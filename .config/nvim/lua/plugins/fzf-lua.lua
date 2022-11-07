e06984068e2938a5d841d779ab8df7f1ca954698
egargan <edwgargan@gmail.com>
2022-11-07 18:18:17 +0000

    feat(nvim): have fzf search from current NERDTree dir

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,8 +1,27 @@
 -- In-nvim FZF
 
+local function isBufferNERDTree()
+  return vim.bo.filetype == 'nerdtree' and
+    vim.api.nvim_eval([[has_key(g:NERDTreeDirNode.GetSelected(), 'path')]])
+end
+
+local function NERDTreeAwareCwd()
+  if isBufferNERDTree() then
+    return vim.api.nvim_eval([[g:NERDTreeDirNode.GetSelected().path.str()]])
+  else
+    return vim.fn.getcwd()
+  end
+end
+
 local function setup()
   local fzf_lua = require('fzf-lua')
   local actions = require('fzf-lua.actions')
+
+  local function NERDTreeAwareEditAction(selected, opts)
+    -- TODO: atm this just moves one buffer to the right, can we change this to most recent buffer?
+    if isBufferNERDTree() then vim.cmd('wincmd w') end
+    actions.file_edit_or_qf(selected, opts)
+  end
 
   local opts = { noremap = true, silent = true }
 
@@ -17,25 +36,41 @@ local function setup()
         layout = 'vertical',
       }
     },
+    cwd = NERDTreeAwareCwd(),
+    actions = {
+      ['default'] = NERDTreeAwareEditAction,
+    }
   }) end, opts)
+
   -- Fuzzy find filenames, including ignored files
   vim.keymap.set('n', '<S-t>', function() fzf_lua.files({
     rg_opts = '--color=never --files --hidden --no-ignore-vcs -g "!.git"',
+    cwd = NERDTreeAwareCwd(),
   }) end, opts)
 
   -- Fuzzy find text
   vim.keymap.set('n', '\\', function() fzf_lua.grep({
     search = '',
+    cwd = NERDTreeAwareCwd(),
+    actions = {
+      ['default'] = NERDTreeAwareEditAction,
+    }
   }) end, opts)
+
   -- Fuzzy find text, including ignored files
   vim.keymap.set('n', '|', function() fzf_lua.grep({
     rg_opts = '--color=never --files --hidden --no-ignore-vcs -g "!.git"',
     search = '',
+    cwd = NERDTreeAwareCwd(),
+    actions = {
+      ['default'] = NERDTreeAwareEditAction,
+    }
   }) end, opts)
 
   -- TODO: can we just fill the prompt vs this weird invisible grep? -- yep! use 'search' param
   -- Fuzzy find text under cursor
   vim.keymap.set('v', '\\', function() fzf_lua.grep_visual() end, opts)
+
   -- Fuzzy find text, including ignored files
   vim.keymap.set('v', '|', function() fzf_lua.grep_visual({
     rg_opts = '--color=never --files --hidden -g "!.git" --no-ignore-vcs'
