9f2989a4704b7d19975ec75cd2a5470e90ed74f7
egargan <edwgargan@gmail.com>
2022-11-11 20:35:57 +0000

    feat(nvim): add Rust LSP config file

---
+++ .config/nvim/lua/lsp/langs/rust.lua
@@ -0,0 +1,8 @@
+-- Rust LSP settings
+
+return function(capabilities, on_attach)
+  require('lspconfig')['rust_analyzer'].setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
