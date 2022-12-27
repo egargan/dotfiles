9105befb558ad125ae0be6db3d19e1724f726771
egargan <edwgargan@gmail.com>
2022-12-27 01:46:55 +0000

    feat(neovim): use nord's prettier diff highlights

--- neovim/.config/nvim/lua/plugins/nord-vim.lua
+++ neovim/.config/nvim/lua/plugins/nord-vim.lua
@@ -3,6 +3,8 @@
 return {
   name = 'arcticicestudio/nord-vim',
   setup = function()
+    -- Disable ugly all-background diff highlight groups
+    vim.g.nord_uniform_diff_background = 1
     vim.cmd('colorscheme nord')
   end,
   priority = 1,
