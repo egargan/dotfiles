d150bb08f64079e0aa53b0f570b654e40db83d7d
egargan <edwgargan@gmail.com>
2022-12-27 19:40:04 +0000

    feat(neovim): extend cursor hl to line number with nord config

--- neovim/.config/nvim/lua/plugins/nord-vim.lua
+++ neovim/.config/nvim/lua/plugins/nord-vim.lua
@@ -5,6 +5,10 @@ return {
   setup = function()
     -- Disable ugly all-background diff highlight groups
     vim.g.nord_uniform_diff_background = 1
+
+    -- Color cursor line number
+    vim.g.nord_cursor_line_number_background = 1
+
     vim.cmd('colorscheme nord')
   end,
   priority = 1,
