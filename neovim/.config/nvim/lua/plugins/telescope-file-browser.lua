7050d5a67a0e5efdf13e926dd54d899d4cc7c227
egargan <edwgargan@gmail.com>
2023-01-29 21:32:41 +0000

    feat(nvim): delete telescope plugin + friends

--- neovim/.config/nvim/lua/plugins/telescope-file-browser.lua
+++
@@ -1,5 +0,0 @@
--- Modal fuzzy finder for everything under the sun
-
-return {
-  name = 'nvim-telescope/telescope-file-browser.nvim',
-}
--- neovim/.config/nvim/lua/plugins/telescope-live-grep-args.lua
+++
@@ -1,5 +0,0 @@
--- Better live_grep builtin with highlighting and rg args
-
-return {
-  name = 'nvim-telescope/telescope-live-grep-args.nvim',
-}
