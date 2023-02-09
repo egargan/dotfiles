0d0944dccde80f956ff24dfba2794c56c8327d84
egargan <edwgargan@gmail.com>
2023-02-09 11:01:06 +0000

    feat(nvim): add persisted plugin

---
+++ neovim/.config/nvim/lua/plugins/persisted.lua
@@ -0,0 +1,11 @@
+-- Automatic session restore and create
+
+return {
+    name = 'olimorris/persisted.nvim',
+    setup = function()
+      require("persisted").setup({
+          use_git_branch = true,
+          autoload = true,
+      })
+    end
+}
