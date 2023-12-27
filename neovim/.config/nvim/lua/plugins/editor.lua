da60ef76c095d87d2bf4485de12a3cb82eaccf82
egargan <edwgargan@gmail.com>
2023-12-27 18:20:18 +0000

    feat(nvim): add structured search + replace (ssr) plugin

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -353,5 +353,32 @@ return {
         desc = "Go to previous reference under cursor"
       },
     }
+  },
+
+  {
+    'cshuaimin/ssr.nvim',
+    keys = {
+      {
+        '<leader>R',
+        function() require("ssr").open() end,
+        mode = { 'n', 'x', 'v' },
+        desc = "Structured search and replace"
+      }
+    },
+    opts = {
+      border = "rounded",
+      min_width = 50,
+      min_height = 5,
+      max_width = 120,
+      max_height = 25,
+      adjust_window = true,
+      keymaps = {
+        close = "q",
+        next_match = "n",
+        prev_match = "N",
+        replace_confirm = "<cr>",
+        replace_all = "<leader><cr>",
+      }
+    }
   }
 }
