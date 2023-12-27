0afdae5ee2995d82ff07c91e3f7ebcb239ee10f9
egargan <edwgargan@gmail.com>
2023-12-27 00:52:36 +0000

    fix(nvim): add 'silent = true' to maps with cmd actions

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -87,14 +87,14 @@ vim.api.nvim_set_keymap('n', '<C-w>/', '<C-w>10>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-w>/', '<C-w>10>', { noremap = true })
 
 -- Speedy tab management
-vim.api.nvim_set_keymap('n', ']t', ':tabnext<Enter>', { noremap = true })
-vim.api.nvim_set_keymap('n', '[t', ':tabprev<Enter>', { noremap = true })
-vim.api.nvim_set_keymap('n', ']T', ':tabnew<Enter>', { noremap = true })
-vim.api.nvim_set_keymap('n', '[T', ':tabclose<Enter>', { noremap = true })
+vim.api.nvim_set_keymap('n', ']t', ':tabnext<Enter>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[t', ':tabprev<Enter>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ']T', ':tabnew<Enter>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[T', ':tabclose<Enter>', { noremap = true, silent = true })
 
 -- Quickfix list navigation
-vim.api.nvim_set_keymap('n', ']q', ':cnext<Enter>', { noremap = true })
-vim.api.nvim_set_keymap('n', '[q', ':cprev<Enter>', { noremap = true })
+vim.api.nvim_set_keymap('n', ']q', ':cnext<Enter>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[q', ':cprev<Enter>', { noremap = true, silent = true })
 
 -- 5-line up/down jumps
 vim.api.nvim_set_keymap('', '<S-k>', '5k', { noremap = true })
@@ -113,18 +113,19 @@ vim.api.nvim_set_keymap('', 'k', 'gk', { noremap = true })
 vim.api.nvim_set_keymap('n', '*', '*``', { noremap = true })
 
 -- Easy remove highlights over search matches
-vim.api.nvim_set_keymap('', '<Bs>', ':noh<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', '<Bs>', ':noh<CR>', { noremap = true, silent = true })
 
 -- s///g macro for word under cursor - applies to whole file in
 -- normal mode, or to selection only in visual mode
-vim.api.nvim_set_keymap('n', '<leader>r', ':%s/\\<<C-r><C-w>\\>//g<left><left>', { noremap = true })
-vim.api.nvim_set_keymap('v', '<leader>r', ':s/\\<<C-r><C-w>\\>//g<left><left>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>r', ':%s/\\<<C-r><C-w>\\>//g<left><left>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>r', ':s/\\<<C-r><C-w>\\>//g<left><left>', { noremap = true, silent = true })
 
 -- Delete everything between cursor and end of above line
-vim.api.nvim_set_keymap('', '<C-j>', ':norm "_d0kgJ"<Enter>', { noremap = true })
+vim.api.nvim_set_keymap('', '<C-j>', ':norm "_d0kgJ"<Enter>', { noremap = true, silent = true })
 
 -- Override default print filename map to also set the system clipboard
-vim.api.nvim_set_keymap('n', '<C-g>', ':let @+ = expand("%")<Enter> <Bar> :echo expand("%")<Enter>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-g>', ':let @+ = expand("%")<Enter> <Bar> :echo expand("%")<Enter>',
+  { noremap = true, silent = true })
 
 -- Keep buffer in bufferlist if it leaves a window
 vim.api.nvim_set_keymap('n', '<C-h>', ':set bufhidden=hide<CR>', { noremap = true, silent = true })
@@ -136,16 +137,13 @@ vim.api.nvim_set_keymap('n', '<C-h>', ':set bufhidden=hide<CR>', { noremap = tru
 vim.api.nvim_set_keymap('n', 'Q', '<Nop>', {})
 
 -- Mapping for closing Vim in a hurry
-vim.api.nvim_set_keymap('n', 'QQ', ':qa!<Enter>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'QQ', ':qa!<Enter>', { noremap = true, silent = true })
 
 -- Easy normal mode in terminal
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
 
 -- Convert snake case to camelcase (temporary)
-vim.api.nvim_set_keymap('v', 'sc', [[:s/\%V_\(\l\)/\u\1/g<Enter>]], { noremap = true })
-
--- Hard reload all listed buffes
-vim.api.nvim_set_keymap('n', '<Leader>E', 'ReloadAllBuffers', { noremap = true })
+vim.api.nvim_set_keymap('v', 'sc', [[:s/\%V_\(\l\)/\u\1/g<Enter>]], { noremap = true, silent = true })
 
 -- Shortcut for yanking to system clipboard
 vim.api.nvim_set_keymap('n', 'Y', '"+y', {})
