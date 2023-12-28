b458a096fdefef84945cdfceb93c927f9b2c67e0
egargan <edwgargan@gmail.com>
2023-12-28 01:17:18 +0000

    feat(nvim): override lua's print function to use nvim-notify

--- neovim/.config/nvim/lua/plugins/core.lua
+++ neovim/.config/nvim/lua/plugins/core.lua
@@ -19,6 +19,7 @@ return {
   {
     -- Pretty notifications
     'rcarriga/nvim-notify',
+    lazy = false,
     opts = {
       render = 'wrapped-compact',
       icons = {
@@ -29,6 +30,31 @@ return {
         TRACE = "•",
       },
       timeout = 3500,
+    },
+    config = function(_, opts)
+      local notify = require("notify")
+
+      vim.notify = notify
+
+      -- Override default print function to use notify
+      print = function(...)
+        local print_safe_args = {}
+        local _ = { ... }
+        for i = 1, #_ do
+          table.insert(print_safe_args, tostring(_[i]))
+        end
+        notify(table.concat(print_safe_args, ' '), "info")
+      end
+
+      require('notify').setup(opts)
+    end,
+    keys = {
+      {
+        '<Bs>',
+        function() require('notify').dismiss() end,
+        mode = { 'n' },
+        desc = 'Dismiss notifications',
+      }
     }
   },
 
