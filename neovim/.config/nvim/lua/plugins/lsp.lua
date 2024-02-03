9e765c77f3c799ae66fc45ddd871f7c57ffa4507
egargan <edwgargan@gmail.com>
2024-02-03 00:03:56 +0000

    feat(nvim): only enable tailwind LSP if config file present

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -117,6 +117,15 @@ return {
             },
           }
         },
+        tailwindcss = {
+          root_dir = function(fname)
+            return require("lspconfig").util.root_pattern(
+              "tailwind.config.cjs",
+              "tailwind.config.js",
+              "postcss.config.js"
+            )(fname)
+          end,
+        },
         yamlls = {
           settings = {
             yaml = { keyOrdering = false }
