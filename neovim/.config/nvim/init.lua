cbbe874ed8db5b25d9217b353c9921799af399ca
egargan <edwgargan@gmail.com>
2024-02-06 23:40:44 +0000

    feat(nvim): open help windows in right vert split

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -184,6 +184,15 @@ vim.api.nvim_create_autocmd({ 'BufModifiedSet', 'TextChangedI', 'TextChanged' },
   end
 })
 
+-- Open help window in a vertical split to the right.
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("help_window_right", {}),
+  pattern = { "*.txt" },
+  callback = function()
+    if vim.o.filetype == 'help' then vim.cmd.wincmd("L") end
+  end
+})
+
 -- Commands ====================================================================
 
 vim.api.nvim_create_user_command('DiagnosticsToggle', function()
